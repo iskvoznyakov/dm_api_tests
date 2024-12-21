ae92dfc037b935bbb9adf0cdc17b188092dea5e2
Ilya Skvoznyakov <iliaskvozniakov@mail.ru>
2024-12-21 10:28:58 +0400

    added client.py and test_account_with_parametrize_with_client.py

---
+++ test_account_with_parametrize_with_client.py
@@ -0,0 +1,36 @@
+import pytest
+from faker import Faker
+from client import Client
+
+@pytest.fixture
+def client():
+    return Client()
+
+fake = Faker("ru_RU")
+for_data = [
+    # Короткий логин
+    {
+        "login": "l",
+        "email": fake.email(),
+        "password": fake.password()
+    },
+    # Невалидный email
+    {
+        "login": fake.user_name(),
+        "email": "login_useremail.ru",
+        "password": fake.password()
+    },
+    # Короткий пароль
+    {
+        "login": fake.user_name(),
+        "email": fake.email(),
+        "password": "1"
+    }
+]
+
+
+@pytest.mark.parametrize('data', for_data)
+def test_post_v1_account_negative(data, client):
+    print(data)
+    response = client.register_user(data)
+    print(response.text)
